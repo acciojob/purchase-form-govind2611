8160718c0a77a1af07a86f843c44de7cb7bcb931
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-11 07:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the stepper elements and continue button
+    const stepper = document.querySelector(".stepper");
+    const stepperItems = stepper.querySelectorAll("li");
+    const continueButton = document.getElementById("continue-button");
+
+    // Add a click event listener to the stepper items
+    stepperItems.forEach((step) => {
+        step.addEventListener("click", function () {
+            // Remove the "selected" class from all steps
+            stepperItems.forEach((item) => item.classList.remove("selected"));
+            
+            // Add the "selected" class to the clicked step
+            step.classList.add("selected");
+        });
+    });
+
+    // Add a click event listener to the continue button
+    continueButton.addEventListener("click", function () {
+        // Find the currently selected step
+        const selectedStep = stepper.querySelector(".selected");
+
+        if (selectedStep) {
+            const stepNumber = selectedStep.getAttribute("data-step");
+            // You can perform an action based on the selected step
+            alert(`Continue to step ${stepNumber}`);
+        }
+    });
+});
